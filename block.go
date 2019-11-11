7162ac4fc111d51289428a19c094dd82d495642d
anzubare <anton.zubarev@t-systems.ru>
2019-11-11 21:31:28 +0300

    Chapter 4

--- block.go
+++ block.go
@@ -5,12 +5,13 @@ import (
 	"bytes"
 	"encoding/gob"
 	"log"
+	"crypto/sha256"
 )
 
 // Block keeps block headers
 type Block struct {
 	Timestamp 		int64
-	Data 			[]byte
+	Transactions	[]*Transaction
 	PrevBlockHash	[]byte
 	Hash 			[]byte
 	Nonce			int
@@ -28,9 +29,22 @@ func (b *Block) Serialize() []byte  {
 	return result.Bytes()
 }
 
+// HashTransactions returns a hash of the transactions in the block
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
+}
+
 // NewBlock creates and returns Block
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0}
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
 
@@ -41,8 +55,8 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 }
 
 // NewGenesisBlock creates and returns genesis Block
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
+func NewGenesisBlock(coinbase *Transaction) *Block {
+	return NewBlock([]*Transaction{coinbase}, []byte{})
 }
 
 func DeserializeBlock(d []byte) *Block {
